9d403b5c07414800442c313fa4993536c9fdab31
edward mccaughan <edwardmccaughan@gmail.com>
2019-08-26 20:34:33 +0200

    tidying up

--- Gemfile
+++ Gemfile
@@ -3,30 +3,14 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.0'
-# Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
-# Use Puma as the app server
 gem 'puma', '~> 3.11'
-# Use SCSS for stylesheets
 gem 'sass-rails', '~> 5'
-# Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
-# Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
-# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
-# Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 4.0'
-# Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use Active Storage variant
-# gem 'image_processing', '~> 1.2'
-
-# Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'devise'
 
 group :development, :test do
   gem 'byebug'
@@ -34,7 +18,6 @@ end
 
 group :development do
   gem 'web-console', '>= 3.3.0'
-  # gem 'listen', '>= 3.0.5', '< 3.2'
 end
 
 group :test do
--- app/controllers/gratitudes_controller.rb
+++ app/controllers/gratitudes_controller.rb
@@ -1,28 +1,20 @@
 class GratitudesController < ApplicationController
   before_action :set_gratitude, only: [:show, :edit, :update, :destroy]
 
-  # GET /gratitudes
-  # GET /gratitudes.json
   def index
     @gratitudes = Gratitude.all
   end
 
-  # GET /gratitudes/1
-  # GET /gratitudes/1.json
   def show
   end
 
-  # GET /gratitudes/new
   def new
     @gratitude = Gratitude.new
   end
 
-  # GET /gratitudes/1/edit
   def edit
   end
 
-  # POST /gratitudes
-  # POST /gratitudes.json
   def create
     @gratitude = Gratitude.new(gratitude_params)
 
@@ -37,8 +29,6 @@ class GratitudesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /gratitudes/1
-  # PATCH/PUT /gratitudes/1.json
   def update
     respond_to do |format|
       if @gratitude.update(gratitude_params)
@@ -51,8 +41,6 @@ class GratitudesController < ApplicationController
     end
   end
 
-  # DELETE /gratitudes/1
-  # DELETE /gratitudes/1.json
   def destroy
     @gratitude.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class GratitudesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_gratitude
       @gratitude = Gratitude.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def gratitude_params
       params.require(:gratitude).permit(:user_id, :body)
     end
