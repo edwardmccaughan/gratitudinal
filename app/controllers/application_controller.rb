17b80a900efedeea4f7463ff209a20d6da92100f
edward mccaughan <edwardmccaughan@gmail.com>
2019-10-19 16:33:01 +0200

    adding current user to exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :prepare_exception_notifier
+
+  def prepare_exception_notifier
+    request.env["exception_notifier.exception_data"] = {
+      current_user: current_user
+    }
+  end
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || vue_app_path
